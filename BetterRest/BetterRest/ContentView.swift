6d7b8c24433df9c801728aa34c720c23b475af56
Alejandro Robles <alejandro_r1704@hotmail.com>
2020-07-01 14:51:14 -0500

    Cleaning up user interface

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeAmount = 1
     
@@ -21,27 +21,29 @@ struct ContentView: View {
     var body: some View {
 
         NavigationView{
-            VStack{
-                Text("¿A que hora quieres despertar?")
-                    .font(.headline)
-                DatePicker("Por favor indica el tiempo",
-                           selection: $wakeUp,
-                           displayedComponents: .hourAndMinute)
-                .labelsHidden()
+            Form {
+                    Text("¿A que hora quieres despertar?")
+                        .font(.headline)
+                    DatePicker("Por favor indica el tiempo",
+                               selection: $wakeUp,
+                               displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+//                    .datePickerStyle(WheelDatePickerStyle())
+                    
+                    Text("Cantidad deseada de sueño")
+                        .font(.headline)
+                    
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25){
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                    }
+                    
+                    Text("Ingesta diaria de café")
+                        .font(.headline)
+                    
+                    Stepper(value: $coffeAmount, in: 1...20){
+                            Text(coffeAmount == 1 ? "1 Taza" : "\(coffeAmount) Tazas")
+                    }
                 
-                Text("Cantidad deseada de sueño")
-                    .font(.headline)
-                
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25){
-                    Text("\(sleepAmount, specifier: "%g") hours")
-                }
-                
-                Text("Ingesta diaria de café")
-                    .font(.headline)
-                
-                Stepper(value: $coffeAmount, in: 1...20){
-                        Text(coffeAmount == 1 ? "1 Taza" : "\(coffeAmount) Tazas")
-                }
             }
             .navigationBarTitle("Better Rest")
             .navigationBarItems(trailing:
@@ -53,6 +55,13 @@ struct ContentView: View {
                 Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("Ok")))
             }
         }
+    }
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
     }
     
     func calculateBedtime(){
